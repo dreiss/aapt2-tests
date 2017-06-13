0cce20f3865b6f6f4d08af94f5fb97463180db67
David Reiss <dreiss@fb.com>
2017-06-12 21:20:12 -0700

    Update style and styleable merge test cases

--- style-merge/run.sh
+++ style-merge/run.sh
@@ -3,14 +3,35 @@ set -e
 : ${AAPT:=aapt}
 : ${AAPT2:=aapt2}
 
+check() {
+  local SYMBOL="$1"
+  local DIR="$2"
+  local RDOT="$3"
+  grep -q "int $SYMBOL" r-dot/$DIR/$RDOT || {
+    echo >&2 "Did not find $SYMBOL in $DIR output."
+    exit 1
+  }
+}
+
 # aapt (1) merges style items
-$AAPT package --auto-add-overlay -f -F aapt.ap_ -M AndroidManifest.xml -S res1 -S res2
+$AAPT package --auto-add-overlay -f -F aapt.ap_ -M AndroidManifest.xml -S res1 -S res2 -J r-dot/aapt
 $AAPT dump --values resources aapt.ap_ > aapt.txt
+check mywidth aapt R.java
+check myheight aapt R.java
+check s1_mywidth aapt R.java
+check s2_myheight aapt R.java
 
 # aapt2 just picks one declaration.
 $AAPT2 compile -o res1.flata --dir res1
 $AAPT2 compile -o res2.flata --dir res1
-$AAPT2 link --auto-add-overlay -o aapt2.ap_ --manifest AndroidManifest.xml  -R res1.flata -R res2.flata
+$AAPT2 link --auto-add-overlay -o aapt2.ap_ --manifest AndroidManifest.xml  -R res1.flata -R res2.flata --java r-dot/aapt2
 $AAPT dump --values resources aapt2.ap_ > aapt2.txt
+check mywidth aapt2 com/example/R.java
+check myheight aapt2 com/example/R.java
+check s1_mywidth aapt2 com/example/R.java
+check s2_myheight aapt2 com/example/R.java
 
+# Verify the resource bundle.
+# Note that this won't run unless all of the R.java checks above pass.
+# Comment them out to force this to run.
 ./verify.py aapt.txt aapt2.txt
--- styleable-merge/run.sh
+++ styleable-merge/run.sh
@@ -3,26 +3,39 @@ set -e
 : ${AAPT:=aapt}
 : ${AAPT2:=aapt2}
 
+check_rdot() {
+  local SYMBOL="$1"
+  local DIR="$2"
+  local RDOT="$3"
+  grep -q "int $SYMBOL" r-dot/$DIR/$RDOT || {
+    echo >&2 "Did not find $SYMBOL in $DIR R.java output."
+    exit 1
+  }
+}
+
+check_bundle() {
+  local SYMBOL="$1"
+  local BUNDLE="$2"
+  grep -q "spec resource 0x[0-9a-f]* com.example:attr/$SYMBOL:" "${BUNDLE}.txt" || {
+    echo >&2 "Did not find $SYMBOL in $BUNDLE bundle output."
+    exit 1
+  }
+}
+
 # aapt (1) merges styleable attributes.
-$AAPT package --auto-add-overlay -M AndroidManifest.xml -S res1 -S res2 -J r-dot/aapt --output-text-symbols r-dot/aapt
-grep -q 'int head' r-dot/aapt/R.java || {
-  echo >&2 'Did not find head in aapt output.'
-  exit 1
-}
-grep -q 'int tail' r-dot/aapt/R.java || {
-  echo >&2 'Did not find tail in aapt output.'
-  exit 1
-}
+$AAPT package --auto-add-overlay -f -F aapt.ap_ -M AndroidManifest.xml -S res1 -S res2 -J r-dot/aapt
+$AAPT dump --values resources aapt.ap_ > aapt.txt
+check_rdot head aapt R.java
+check_rdot tail aapt R.java
+check_bundle head aapt
+check_bundle tail aapt
 
 # aapt2 just picks one declaration.
 $AAPT2 compile -o res1.flata --dir res1
 $AAPT2 compile -o res2.flata --dir res1
-$AAPT2 link --auto-add-overlay -o r-dot/aapt2/res.ap_ --manifest AndroidManifest.xml  -R res1.flata -R res2.flata --java r-dot/aapt2
-grep -q 'int head' r-dot/aapt2/com/example/R.java || {
-  echo >&2 'Did not find head in aapt2 output.'
-  exit 1
-}
-grep -q 'int tail' r-dot/aapt2/com/example/R.java || {
-  echo >&2 'Did not find tail in aapt2 output.'
-  exit 1
-}
+$AAPT2 link --auto-add-overlay -o aapt2.ap_ --manifest AndroidManifest.xml  -R res1.flata -R res2.flata --java r-dot/aapt2
+$AAPT dump --values resources aapt2.ap_ > aapt2.txt
+check_rdot head aapt2 com/example/R.java
+check_rdot tail aapt2 com/example/R.java
+check_bundle head aapt2
+check_bundle tail aapt2
